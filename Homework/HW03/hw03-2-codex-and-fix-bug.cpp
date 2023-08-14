50ae3cc518e608a17d4006879944410ce2932f4d
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-14 15:59:12 +0700

    update hw3-2

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -4,28 +4,28 @@
 #include <stdbool.h> // เพิ่มเพื่อใช้งาน bool ใน C
 
 int main() {
-    float a;
-    float b = 3; // กำหนดค่าเริ่มต้นให้ b
-    bool _1 = false; // แก้ไขเป็น false
-    printf("%d ", _1);
+    float a ;
+    float b = 3 ; // กำหนดค่าเริ่มต้นให้ b
+    bool _1 = false ; // แก้ไขเป็น false
+    printf( "%d", _1 ) ;
     int i = 0;
 
-    if (_1 != false) {
-        printf("ok ");
+    if ( _1 != false ) {
+        printf( "ok " ) ;
     } else {
-        while (i <= 5) {
-            printf("not-okey-%d ", ++i); // แก้ไขเป็น not-okay
+        while ( i <= 5 ) {
+            printf( "not-okey- %d ", ++i ) ; // แก้ไขเป็น not-okay
         }
         
-        for (i = 3; i < 10; i += 1) {
-            if (i % 2 == (!_1 ? 1 : 2)) {
-                printf("see see");
+        for ( i = 3; i < 10; i += 1 ) {
+            if (i % 2 == ( !_1 ? 1 : 2 ) ) {
+                printf( "see see" ) ;
             } else {
-                printf("haha");
+                printf( "haha" ) ;
             }//if else
         }//end for
     }//end if else
 
-    return 0;
+    return 0 ;
 }//end function
 
