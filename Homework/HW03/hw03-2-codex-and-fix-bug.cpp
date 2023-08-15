edd921a09a3963a26ae0b46cc65731f412995527
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-15 18:52:14 +0700

    update hw3-2

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,31 +1,27 @@
-//TODO: #9 Fix bug and arrange code in CODEX pattern.
-
 #include <stdio.h>
-#include <stdbool.h> // เพิ่มเพื่อใช้งาน bool ใน C
 
 int main() {
-    float a ;
-    float b = 3 ; // กำหนดค่าเริ่มต้นให้ b
-    bool _1 = false ; // แก้ไขเป็น false
-    printf( "%d", _1 ) ;
+    float a, b = 3;
+    int _1 = 0;
+    
+    printf("%d ", _1);
+    
     int i = 0;
-
-    if ( _1 != false ) {
-        printf( "ok " ) ;
+    if (_1 != 0) {
+        printf("ok ");
     } else {
-        while ( i <= 5 ) {
-            printf( "not-okey- %d ", ++i ) ; // แก้ไขเป็น not-okay
+        while (i <= 5) {
+            printf("not-okay-%d ", ++i);
         }
         
-        for ( i = 3; i < 10; i += 1 ) {
-            if (i % 2 == ( !_1 ? 1 : 2 ) ) {
-                printf( "see see" ) ;
+        for (i = 3; i < 10; i += 1) {
+            if (i % 2 == (!_1 ? 1 : 2)) {
+                printf("see see ");
             } else {
-                printf( "haha" ) ;
-            }//if else
-        }//end for
-    }//end if else
-
-    return 0 ;
-}//end function
-
+                printf("haha ");
+            }
+        }
+    }
+    
+    return 0;
+}
