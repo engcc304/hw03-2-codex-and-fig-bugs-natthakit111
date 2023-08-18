4e852fa9c977b892d517273ee97e3874a3e4616a
natthakit111 <natthakit_ya66@live.ac.th>
2023-08-18 23:45:48 +0700

    update hw3-2

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    float a, b = 3;
-    int _1 = 0;
+    float a, b = 3 ;
+    int _1 = 0 ;
     
-    printf("%d ", _1);
+    printf(" %d ", _1 ) ; 
     
-    int i = 0;
-    if (_1 != 0) {
-        printf("ok ");
+    int i = 0 ;
+    if ( _1 != 0 ) {
+        printf( "ok " ) ;
     } else {
-        while (i <= 5) {
-            printf("not-okay-%d ", ++i);
+        while ( i <= 5 ) {
+            printf( "not-okay-%d ", ++i ) ;
         }
         
-        for (i = 3; i < 10; i += 1) {
-            if (i % 2 == (!_1 ? 1 : 2)) {
-                printf("see see ");
+        for ( i = 3; i < 10; i += 1 ) {
+            if ( i % 2 == ( !_1 ? 1 : 2 ) ) {
+                printf( "see see " ) ;
             } else {
-                printf("haha ");
-            }
-        }
-    }
+                printf( "haha " ) ;
+            }//if else
+        }//for
+    }//if else 
     
-    return 0;
-}
+    return 0 ;
+}//end function
